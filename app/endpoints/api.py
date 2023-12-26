61e963435d5121ee533df2dd2237bb38ef82abfb
glejz <lexa.glejzer@yandex.ru>
2023-12-26 15:53:50 +0300

    bonus_points improved

--- app/endpoints/api.py
+++ app/endpoints/api.py
@@ -162,7 +162,7 @@ async def process_cards(request, cards: list[schemas.DiscountCard]) -> Response:
     return cards_response
 
 
-async def update_transaction(transaction: schemas.BonusTransaction) -> Result:
+async def update_or_create_transaction(transaction: schemas.BonusTransaction) -> Result:
     try:
         for bonus_operation in transaction.bonus_points:
             discount_card_enote_id = bonus_operation.discount_card_enote_id
@@ -180,15 +180,17 @@ async def update_transaction(transaction: schemas.BonusTransaction) -> Result:
             else:
                 sum = bonus_operation.sum
                 if sum > 0:
-                    sum = -abs(sum)
+                    sum = -sum
         card = await sync_to_async(
             lambda: get_object_or_404(DiscountCard, enote_id=discount_card_enote_id)
         )()
-        await BonusTransaction.objects.acreate(
+        await BonusTransaction.objects.aupdate_or_create(
             enote_id=transaction.enote_id,
-            datetime=datetime,
-            discount_card=card,
-            sum=sum,
+            defaults={
+                "datetime": datetime,
+                "discount_card": card,
+                "sum": sum,
+            }
         )
         return Result(
             enote_id=transaction.enote_id,
@@ -208,5 +210,5 @@ async def process_transactions(
 ) -> Response:
     balance_response = Response(response=[])
     for transaction in transactions:
-        balance_response.response.append(await update_transaction(transaction))
+        balance_response.response.append(await update_or_create_transaction(transaction))
     return balance_response
