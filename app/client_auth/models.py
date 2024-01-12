12e88c8d4583b6f6219810b52daf202d3c70a92d
Aleksei Gleizer <gleizer.a@divan.ru>
2024-01-12 08:54:39 +0000

    Update models.py

--- app/client_auth/models.py
+++ app/client_auth/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models import Sum
 from asgiref.sync import sync_to_async
-from bonuses.models import DiscountCard, BonusTransaction
 
 
 class Client(models.Model):
