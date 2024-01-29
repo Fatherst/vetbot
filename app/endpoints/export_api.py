16c9d0f90832f70161056f8dbed3fe8602b3009f
glejz <lexa.glejzer@yandex.ru>
2024-01-29 17:44:42 +0300

    mr improvs except period in env

---
+++ app/endpoints/export_api.py
@@ -0,0 +1,77 @@
+import csv
+import io
+from datetime import datetime
+from django.utils import timezone
+import pytz
+
+from asgiref.sync import sync_to_async
+from django.http import HttpResponse, StreamingHttpResponse
+from ninja import Router
+import logging
+
+from appointment import models as appointment_models
+
+
+logger = logging.getLogger(__name__)
+
+
+export_router = Router()
+
+
+@export_router.get("invoices")
+async def export_csv(request) -> HttpResponse:
+    csv_buffer = io.StringIO()
+    writer = csv.writer(csv_buffer)
+
+    fieldnames = [
+        "Дата и время оплаты",
+        "Id клиента в системе Enote",
+        "Эл.почта клиента",
+        "Мобильный телефон клиента",
+        "Общая итоговая сумма оплаты",
+        "Новый клиент",
+    ]
+    writer.writerow(fieldnames)
+    start_date = datetime(2024, 1, 8, tzinfo=pytz.timezone('Europe/Moscow'))
+    # Получаем данные из базы данных
+    invoices = await sync_to_async(list)(
+        appointment_models.Invoice.objects.filter(
+            date__range=[start_date, timezone.now()]
+        )
+        .select_related("client")
+        .all()
+    )
+    for invoice in invoices:
+        # Проверяем, является ли инвойс первым у клиента
+        is_first_invoice = not await sync_to_async(
+            (appointment_models.Invoice.objects.filter)(
+                client=invoice.client, date__lt=invoice.date
+            ).exists
+        )()
+        phone_number = invoice.client.phone_number
+        if phone_number[0] == "8":
+            phone_number = f"7{phone_number[1:]}"
+        email = invoice.client.email
+        # csv_data = {
+        #     "Дата и время оплаты": invoice.date.strftime("%d.%m.%Y %H:%M"),
+        #     "Id клиента в системе Enote": invoice.client.enote_id,
+        #     "Эл.почта клиента": email,
+        #     "Мобильный телефон клиента": phone_number,
+        #     "Общая итоговая сумма оплаты": invoice.sum,
+        #     "Новый клиент": "Да" if is_first_invoice else "Нет",
+        # }
+        csv_data = [
+            invoice.date.strftime("%d.%m.%Y %H:%M"),
+            invoice.client.enote_id,
+            email,
+            phone_number,
+            invoice.sum,
+            "Да" if is_first_invoice else "Нет",
+        ]
+        writer.writerow(csv_data)
+    csv_content = csv_buffer.getvalue()
+    csv_buffer.close()
+    response = HttpResponse(csv_content, content_type="text/csv")
+    response["Content-Disposition"] = (f'attachment; filename="invoices_'
+                                       f'{timezone.now().strftime("%Y_%m_%d")}.csv"')
+    return response
