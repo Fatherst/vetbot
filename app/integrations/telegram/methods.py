2a7a9089252455e002ff322a53466348ad154984
glejz <lexa.glejzer@yandex.ru>
2024-03-12 15:33:37 +0300

    Message after accrual fixed

--- app/integrations/telegram/methods.py
+++ app/integrations/telegram/methods.py
@@ -5,13 +5,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def send_message(chat_id: int, text: str):
+def send_message(chat_id: int, text: str, reply_markup):
     try:
         requests.get(
             f"https://api.telegram.org/bot{settings.BOT_API_TOKEN}/sendMessage",
             params={
                 "chat_id": chat_id,
                 "text": text,
+                "reply_markup": reply_markup
             },
         )
     except Exception as error:
